05b1f8742d942e4fafb6eab00835452bde5392fa
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-31 23:20:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
   arrayofvalues= []
 
@@ -19,6 +19,7 @@ def key_for_min_value(name_hash)
       #arrayofvalues[i], arrayofvalues[i+1] = arrayofvalues[i+1], arrayofvalues[i]
 
     i+=1
+    binding.pry
   end
   return name_hash.key(value)
 
